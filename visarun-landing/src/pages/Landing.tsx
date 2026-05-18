64ecf90f223e2bc8b8addcaadeba0f6fdd9063b4
tungyr-one <victor.tungusov@gmail.com>
2026-05-18 15:04:42 +0200

    Deploying stuff

--- visarun-landing/src/pages/Landing.tsx
+++ visarun-landing/src/pages/Landing.tsx
@@ -1,17 +1,21 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Features from "../components/Features";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+// import Navbar from "../components/Navbar";
+// import Hero from "../components/Hero";
+// import Features from "../components/Features";
+// import Contact from "../components/Contact";
+// import Footer from "../components/Footer";
+// import { useState } from "react";
 
-export default function Landing() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Features />
-      <Contact />
-      <Footer />
-    </>
-  );
-}
+// export default function Landing() {
+// const [lang, setLang] = useState("en");
+// const [dark, setDark] = useState(false);
+
+//   return (
+//     <>
+//       <Navbar />
+//       <Hero />
+//       <Features />
+//       <Contact />
+//       <Footer />
+//     </>
+//   );
+// }
